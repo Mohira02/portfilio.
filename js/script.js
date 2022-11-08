90421bd294c1626b3ba3c19949c9cab6acac64aa
Mohira02 <mohirabobonazarova73@gmail.com>
2022-11-08 14:52:39 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $('#menu2').click(function () {
+        $('#offcanvasDarkNavbar').removeClass('show');
+    })
+    // $('header').toggleClass('toggle');
+    //     });
+    //     $(window).on('scroll load',function(){
+    //         $('#menu')
+    // $('header').removeClass('toggle');
+
+    // if($(window).scrollTop() > 0){
+    //     $('.top').show();
+    // }else{
+    //     $('.top').hide();
+    // }
+
+    //     });
+
+    //     // smooth scrolling
+    //     $('a[href*="#"]').on('click',function(e){
+    //         e.preventDefault();
+    //         $('html, body').animate({
+    //             scrollTop : $($(this).attr('href')).offset().top,
+
+    //         },
+    //         500,
+    //         'linear'
+    //         )
+
+    //     })
+
+
+})
